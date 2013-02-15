0692122225a1d12315c5ed5697dbf725d3733d36
Santtu Lintervo <santervo@gmail.com>
2013-02-15 22:07:19 +0200

    Export defineModel

--- test/model_test.js
+++ test/model_test.js
@@ -1,30 +1,35 @@
-var Model = require('../lib/model');
+var mongorecord = require('../');
 
 describe('Model', function() {
-  var Issue = Model.define({
-    title: String,
-    scheduled_date: Date,
-    updated_at: {
-      type: Date,
-      protected: true
+
+  //mongorecord.connect("http://localhost/mongorecord_test");
+
+  var User = mongorecord.defineModel({
+    properties: {
+      name: String,
+      birthDate: Date,
+      updatedAt: {
+        type: Date,
+        protected: true
+      }
     }
   });
 
   describe('set', function() {
     it('should sanitize data', function() {
-      var obj = new Issue();
+      var obj = new User();
 
       obj.set({
-        title: 'Bar',
-        foo: 'Foo',
-        scheduled_date: '2012-01-01T10:00:00Z',
-        updated_at: '2012-01-01T10:00:00Z'
+        name: 'John Doe',
+        password: 'secret',
+        birthDate: '1980-05-01T10:00:01Z',
+        updated_at: '2012-01-01T12:00:00Z'
       });
 
-      assert.equal('Bar', obj.get('title'));
-      assert.equal(undefined, obj.get('foo'));
-      assert.equal(new Date('2012-01-01T10:00:00Z').getTime(), obj.get('scheduled_date').getTime());
-      assert.equal(undefined, obj.get('updated_at'));
+      assert.equal('John Doe', obj.get('name'));
+      assert.equal(undefined, obj.get('password'));
+      assert.equal(new Date('1980-05-01T10:00:01Z').getTime(), obj.get('birthDate').getTime());
+      assert.equal(undefined, obj.get('updatedAt'));
     });
   });
 });
